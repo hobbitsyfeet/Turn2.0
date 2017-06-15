2ddb14dde5ddfbf1fe3188046fa4c073fd5f1fbc
Justin Petluk <justinpetluk@Justin-Petluks-MacBook-Pro.local>
2017-06-14 18:31:35 -0600

    deleted gtest and added OpenGL/GLUT

--- main.cc
+++ main.cc
@@ -1,7 +1,42 @@
-#include "iostream"
-using namespace std;
+#include <GL/freeglut.h>
 
-int main(){
-	cout<<"THIS IS MAIN";
-	return 0;
+
+void draw(void) {
+
+    // Black background
+    glClearColor(0.0f,0.0f,0.0f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    //Draw i
+    glFlush();
+
+}
+
+//Main program
+
+int main(int argc, char **argv) {
+
+    glutInit(&argc, argv);
+
+    /*Setting up  The Display
+    /    -RGB color model + Alpha Channel = GLUT_RGBA
+    */
+    glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
+
+    //Configure Window Postion
+    glutInitWindowPosition(50, 25);
+
+    //Configure Window Size
+    glutInitWindowSize(480,480);
+
+    //Create Window
+    glutCreateWindow("Hello OpenGL");
+
+
+    //Call to the drawing function
+    glutDisplayFunc(draw);
+
+    // Loop require by OpenGL
+    glutMainLoop();
+
+    return 0;
 }
