74bf33cdad0c2d433015c8f63d69824360b37377
Justin <hobbitsyfeet@gmail.com>
2018-01-02 13:15:06 -0700

    Delete old code.
    -Old code was previously designed to work only with Terminal.
    -Old code used bad design. No research was used while implementing code.

--- main.cc
+++ main.cc
@@ -1,43 +1,5 @@
-#include <GLFW/glfw3.h>
-#include "GLModel.h"
-#include "GLView.h"
-#include"Window.h"
 #include <iostream>
 
 
 int main(int argc, char **argv) {
-	GLModel model;
-
-	rectangle rect(1,1);
-	/* Initialize the library */
-	if (!glfwInit())
-	return -1;
-
-	//Create window class
-	//Takes care of initialization and also creates a glfwWindow
-	Window window(480,640,"Turn");
-	if (!window.getWindow())
-	{
-		glfwTerminate();
-		//exit(EXIT_FAILURE);
-	}
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window.getWindow());
-	model.init();
-	model.setViewport(window.getWidth()*2,window.getHeight());
-			window.resizeWindow(5);
-
-	while (!glfwWindowShouldClose(window.getWindow()))
-	{
-		//double last = glfwGetTime();
-		window.renderScene();
-
-		model.display();
-		generateShapes();
-
-	}
-	/* Loop until the user closes the window */
-	glfwTerminate();
-	return 0;
-
 }
